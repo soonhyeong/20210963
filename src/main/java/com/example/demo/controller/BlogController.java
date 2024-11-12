65fa3a1703458bdaf6b554d0bb630da1e6a8bca4
asd <asd@asd>
2024-11-12 09:07:35 +0900

    9주차

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.example.demo.model.domain.Article;
+import com.example.demo.model.domain.Board;
 import com.example.demo.model.service.BlogService;
 import com.example.demo.model.service.AddArticleRequest;
+import com.example.demo.model.service.AddBoardRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,43 +31,95 @@ public class BlogController {
     @Autowired
     private BlogService blogService;
 
-    @GetMapping("/article_list")
-    public String article_list(Model model) {
-        List<Article> list = blogService.findAll(); // 게시판 리스트
+    // @GetMapping("/article_list")
+    // public String article_list(Model model) {
+    //     List<Article> list = blogService.findAll(); // 게시판 리스트
+    //     model.addAttribute("articles", list); // 모델에 추가
+    //     return "article_list";
+    // }
+
+    @GetMapping("/board_list") // 새로운 게시판 링크 지정
+    public String board_list(Model model) {
+        List<Board> list = blogService.findAll(); // 게시판 전체 리스트
         model.addAttribute("articles", list); // 모델에 추가
-        return "article_list";
+        return "board_list"; // .HTML 연결
     }
 
-    @PostMapping("/articles")
-    public String addArticle(@ModelAttribute AddArticleRequest request) {
-        blogService.save(request);
-        return "redirect:/article_list";
-    }
-
-    @GetMapping("/article_edit/{id}") // 게시판 링크 지정
-    public String article_edit(Model model, @PathVariable Long id) {
-        Optional<Article> list = blogService.findById(id); // 선택한 게시판 글
-
+    @GetMapping("/board_view/{id}") // 게시판 링크 지정
+    public String board_view(Model model, @PathVariable Long id) {
+        Optional<Board> list = blogService.findById(id); // 선택한 게시판 글
         if (list.isPresent()) {
-            model.addAttribute("article", list.get()); // 존재하면 Article 객체를 모델에 추가
-        }
-        else {
+            model.addAttribute("boards", list.get()); // 존재할 경우 실제 Article 객체를 모델에 추가
+        } else {
             // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
             return "/error_page/article_error"; // 오류 처리 페이지로 연결
         }
-        return "article_edit"; // .HTML 연결
+        return "board_view"; // .HTML 연결
     }
 
-    @PutMapping("/api/article_edit/{id}")
-    public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
+
+    // @PostMapping("/articles")
+    // public String addArticle(@ModelAttribute AddArticleRequest request) {
+    //     blogService.save(request);
+    //     return "redirect:/article_list";
+    // }
+
+    // @GetMapping("/article_edit/{id}") // 게시판 링크 지정
+    // public String article_edit(Model model, @PathVariable Long id) {
+    //     Optional<Article> list = blogService.findById(id); // 선택한 게시판 글
+
+    //     if (list.isPresent()) {
+    //         model.addAttribute("article", list.get()); // 존재하면 Article 객체를 모델에 추가
+    //     }
+    //     else {
+    //         // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
+    //         return "/error_page/article_error"; // 오류 처리 페이지로 연결
+    //     }
+    //     return "article_edit"; // .HTML 연결
+    // }
+
+    // @PutMapping("/api/article_edit/{id}")
+    // public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
+    //     blogService.update(id, request);        
+    //     return "redirect:/article_list"; // 글 수정 이후 .html 연결
+    // }
+
+    // @DeleteMapping("/api/article_edit/{id}")
+    // public String deleteArticle(@PathVariable Long id) {
+    //     blogService.delete(id);
+    //     return "redirect:/article_list";
+    // }
+
+    @PostMapping("/boards")
+    public String addBoard(@ModelAttribute AddBoardRequest request) {
+        blogService.save(request);
+        return "redirect:/board_list";
+    }
+
+    @GetMapping("/board_edit/{id}") // 게시판 링크 지정
+    public String board_edit(Model model, @PathVariable Long id) {
+        Optional<Board> list = blogService.findById(id); // 선택한 게시판 글
+
+        if (list.isPresent()) {
+            model.addAttribute("board", list.get()); // 존재하면 Article 객체를 모델에 추가
+        }
+        else {
+            // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
+            return "/error_page/board_error"; // 오류 처리 페이지로 연결
+        }
+        return "board_edit"; // .HTML 연결
+    }
+
+    @PutMapping("/api/board_edit/{id}")
+    public String updateBoard(@PathVariable Long id, @ModelAttribute AddBoardRequest request) {
         blogService.update(id, request);        
-        return "redirect:/article_list"; // 글 수정 이후 .html 연결
+        return "redirect:/board_list"; // 글 수정 이후 .html 연결
     }
 
-    @DeleteMapping("/api/article_edit/{id}")
-    public String deleteArticle(@PathVariable Long id) {
+    @DeleteMapping("/api/board_edit/{id}")
+    public String deleteBoarde(@PathVariable Long id) {
         blogService.delete(id);
-        return "redirect:/article_list";
+        return "redirect:/board_list";
     }
     
     @GetMapping("/favicon.ico")
@@ -78,13 +132,13 @@ public class BlogController {
     @ExceptionHandler(NumberFormatException.class)
     public String handleNumberFormatException(NumberFormatException ex, Model model) {
         model.addAttribute("errorMessage", "잘못된 요청입니다. ID는 정수여야 합니다.");
-        return "article_error"; // 오류 페이지로 리다이렉트 (템플릿 이름)
+        return "board_error"; // 오류 페이지로 리다이렉트 (템플릿 이름)
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoHandlerFoundException.class)
     public String handleNoHandlerFoundException(NoHandlerFoundException ex, Model model) {
         model.addAttribute("errorMessage", "요청한 페이지를 찾을 수 없습니다.");
-        return "article_error"; // 오류 페이지로 리다이렉트
+        return "board_error"; // 오류 페이지로 리다이렉트
     }
 }
